591c3a5a087fb6ef6ccd288c973ea8289f4c2b58
fhiuhiufweflhbwffwefewjfbweifbwniwfnrwebfrwbfwbfejfbekwf <league@iMac.attlocal.net>
2016-08-20 12:03:12 -0700

    oncogeneDectector

--- src/OncogeneDetector.java
+++ src/OncogeneDetector.java
@@ -1,50 +1,68 @@
 import java.util.ArrayList;
 
 public class OncogeneDetector {
-	public boolean isOncogene(ArrayList<String> healthySequences, ArrayList<String> cancerSequence, String Candidate) {
+	public boolean isOncogene(ArrayList<String> healthySequences, ArrayList<String> cancerSequence, String Candidate)  {
 		boolean isCancer = false;
 		float cancer = 0;
 		float total = 0;
-		String word = "";
+		if(Candidate.matches("[TGCA]*")==false)
+		{
+			throw new IllegalArgumentException("candidate must contain only the letters A, C, G or T");
+				}
+		
+		if(healthySequences.size()==0)
+		{
+			
+				throw new IllegalArgumentException("healthySequences must contain at least one element");
+			
+		}
+			
 		for (String letter : healthySequences) {
-			if (letter.equals("")) {
-				if (word.contains(Candidate)) {
+			//System.out.println(letter);
+			if(letter.matches("[TGCA]*")==false)
+			{
+				throw new IllegalArgumentException();
+			}
+				if( letter.contains(Candidate)) {
 					cancer++;
 
-				} else {
-					word = "";
+				} 
 
-				}
-
-			} else {
-				word = word + letter + " ";
-			}
+			
 			total++;
 		}
 
 		// cancer sequence begins
 		float cancer2 = 0;
 		float total2 = 0;
-		String word2 = "";
+		if(cancerSequence.size()==0)
+		{
+			
+				throw new IllegalArgumentException("cancerSequences must contain at least one element");
+			
+		}
 		for (String letter2 : cancerSequence) {
-			if (letter2.equals("")) {
-				if (word.contains(Candidate)) {
+			if(letter2.matches("[TGCA]*")==false)
+			{
+				throw new IllegalArgumentException();
+			}
+			System.out.println(letter2);
+				if (letter2.contains(Candidate)) {
 					cancer2++;
 
-				} else {
-					word2 = "";
-
-				}
-
-			} else {
-				word2 = word2 + letter2 + " ";
-			}
+				} 
 			total2++;
 		}
 		// cancer ends
+		System.out.println(cancer2);
+		System.out.println(total2);
 		if ((cancer / total) > (cancer2 / total2)) {
 			isCancer = false;
-		} else {
+		} 
+		else if ((cancer / total) == (cancer2 / total2)) {
+			isCancer=false;
+		}
+		else {
 			isCancer = true;
 		}
 
